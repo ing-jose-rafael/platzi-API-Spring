b92f05078b3c8150d934d8f62bf50b0ebe2fe2fd
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-03-02 11:14:44 -0500

    creando la clsae servicio y su anotacion

---
+++ src/main/java/com/platzi/platzimarket/domain/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.platzi.platzimarket.domain.service;
+
+import com.platzi.platzimarket.domain.Product;
+import com.platzi.platzimarket.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll(){
+        return productRepository.getAll();
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId){
+        return productRepository.getByCategory(categoryId);
+    }
+//    Optional<List<Product>> getScarseProducts(int quantity);
+    public Optional<Product> getProduct(int productId){
+        return productRepository.getProduct(productId);
+    }
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+    public boolean delete(int productId){
+        return getProduct(productId).map(product -> {
+            productRepository.delete(product.getProductId());
+            return true;
+        }).orElse( false);
+
+        /*segunda opcion*/
+        /*
+        if (getProduct(productId).isPresent()){
+            productRepository.delete(productId);
+            return true;
+        }
+        return false;*/
+    }
+}
