9b08d2dcd64aa9048baef0f82cb095b9bde1dd01
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-03-01 20:30:48 -0500

    agregando anotacion repository

--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -2,20 +2,30 @@ package com.platzi.platzimarket.persistence;
 
 import com.platzi.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository  {
     private  ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getProductos(){
         return productoCrudRepository.findAll();
     }
+
     public List<Producto> getByIdCategoria(int idCategoria){
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
     }
+
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
     }
+
+    public Optional<Producto> getProducto(int idProducto){ return productoCrudRepository.findById(idProducto);}
+
+    public Producto save(Producto producto){ return productoCrudRepository.save(producto);}
+
+    public void delete(int idProducto){ productoCrudRepository.deleteById(idProducto);}
 }
