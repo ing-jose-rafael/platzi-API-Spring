28085148eb603a104fdc4fab0b5f3c591a0e9c5e
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-03-01 20:18:00 -0500

    query metho buscar menor que una cantidad stock

--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -4,11 +4,18 @@ import com.platzi.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzimarket.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository  {
     private  ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getProductos(){
         return productoCrudRepository.findAll();
+    }
+    public List<Producto> getByIdCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
     }
 }
--- src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,11 @@ package com.platzi.platzimarket.persistence.crud;
 import com.platzi.platzimarket.persistence.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends JpaRepository<Producto,Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    /* productos que se estan agotando y estado activo*/
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock,boolean estado);
 }
